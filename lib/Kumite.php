64db4e0360b7c3a95bde08dfc66ecd2a526f7b41
Luke Cawood <luke.cawood@99designs.com>
2014-02-06 15:11:00 +1100

    Replace needlessly defined methods in Kumite with a _callStatic

--- lib/Kumite.php
+++ lib/Kumite.php
@@ -6,41 +6,10 @@ class Kumite
 {
     private static $instance;
 
-    // @codeCoverageIgnoreStart
-    public static function start($testKey, $metadata = null)
+    public static function __callStatic($name, $arguments)
     {
         self::assertSetup();
-        self::$instance->start($testKey, $metadata);
-    }
-
-    public static function inTest($testKey)
-    {
-        self::assertSetup();
-        return self::$instance->inTest($testKey);
-    }
-
-    public static function participantId($testKey)
-    {
-        self::assertSetup();
-        return self::$instance->participantId($testKey);
-    }
-
-    public static function event($testKey, $eventKey, $metadata = null)
-    {
-        self::assertSetup();
-        self::$instance->event($testKey, $eventKey, $metadata);
-    }
-
-    public static function eventOffline($testKey, $variantKey, $eventKey, $participantId, $metadata = null)
-    {
-        self::assertSetup();
-        self::$instance->eventOffline($testKey, $variantKey, $eventKey, $participantId, $metadata);
-    }
-
-    public static function variant($testKey)
-    {
-        self::assertSetup();
-        return self::$instance->variant($testKey);
+        return call_user_func_array(array(self::$instance, $name), $arguments);
     }
 
     public static function setup($configuration)
@@ -67,6 +36,4 @@ class Kumite
         if (!isset(self::$instance))
             throw new Exception("Kumite::setup() needs to be called before this method");
     }
-
-    // @codeCoverageIgnoreEnd
 }

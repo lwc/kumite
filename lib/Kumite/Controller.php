9a4a0d387c4581a0953839cb97e868bbaa7ca229
Luke Cawood <luke.cawood@99designs.com>
2014-02-10 10:14:44 +1100

    Add documentation to kumite controller methods

--- lib/Kumite/Controller.php
+++ lib/Kumite/Controller.php
@@ -22,6 +22,22 @@ class Controller
         $this->testConfig = $testConfig;
     }
 
+    /**
+     * Attempts to start a test
+     *
+     * If the allocator returns a variant, a participant is created and a cookie
+     * is set in the response.
+     *
+     * If no variant is returned, the request is considered excluded from the test.
+     * Once a request is participating, further calls to start do nothing.
+     *
+     * The allocator may be overridden for special case tests, such as where
+     * allocation is happening in an external system, or for testcases.
+     *
+     * @param string $testKey the key of the defined test to start
+     * @param array $metadata optional metadata to attach to the participant
+     * @param mixed $allocatorOverride an allocator, or variant key to override the configured allocator
+     */
     public function start($testKey, $metadata = null, $allocatorOverride = null)
     {
         $test = $this->getTest($testKey);
@@ -38,12 +54,27 @@ class Controller
         }
     }
 
+    /**
+     * Resume a test by overriding any set cookie for that test.
+     *
+     * Useful for persisting tests across devices / via email
+     *
+     * @param string $testKey
+     * @param string $variantKey
+     * @param string $participantId
+     */
     public function resume($testKey, $variantKey, $participantId)
     {
         $test = $this->getTest($testKey);
         $this->setCookie($test, $variantKey, $participantId);
     }
 
+    /**
+     * Return the participant identifier for the test.
+     *
+     * @param string $testKey
+     * @return string
+     */
     public function participantId($testKey)
     {
         $test = $this->getTest($testKey);
@@ -53,12 +84,27 @@ class Controller
         }
     }
 
+    /**
+     * Returns true if the request is participating in the test.
+     *
+     * @param string $testKey
+     * @return bool
+     */
     public function inTest($testKey)
     {
         $test = $this->getTest($testKey);
         return (bool) $this->getCookie($test);
     }
 
+    /**
+     * Returns the active variant for the provided test.
+     *
+     * If not participating in the test, the default variant will be returned.
+     * To test for actual participation, use the above inTest method.
+     *
+     * @param string $testKey
+     * @return Variant
+     */
     public function variant($testKey)
     {
         $test = $this->getTest($testKey);
@@ -69,12 +115,30 @@ class Controller
         return $test->variant($cookie['variant']);
     }
 
-    public function eventOffline($testKey, $variantKey, $eventKey, $participantId, $metadata)
+    /**
+     * Tracks an event outside of request context.
+     *
+     * @param string $testKey
+     * @param string $variantKey
+     * @param string $eventKey
+     * @param string $participantId
+     * @param array $metadata
+     */
+    public function eventOffline($testKey, $variantKey, $eventKey, $participantId, $metadata = null)
     {
         $test = $this->getTest($testKey);
         $test->createEvent($variantKey, $eventKey, $participantId, $metadata);
     }
 
+    /**
+     * Tracks an event for participants in the test.
+     *
+     * Does nothing if the request is not participating in the test.
+     *
+     * @param string $testKey
+     * @param string $eventKey
+     * @param array $metadata
+     */
     public function event($testKey, $eventKey, $metadata = null)
     {
         $test = $this->getTest($testKey);
@@ -84,6 +148,11 @@ class Controller
         }
     }
 
+    /**
+     * Returns all defined tests.
+     *
+     * @return array
+     */
     public function getTests()
     {
         foreach (array_keys($this->testConfig) as $testKey) {
@@ -93,6 +162,8 @@ class Controller
     }
 
     /**
+     * Returns the Test object for the given test key.
+     *
      * @param type $testKey
      * @return Test
      */
